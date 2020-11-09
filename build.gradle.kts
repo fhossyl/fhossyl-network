6fac9aec0d3ca43769ebe291256eba014d376f93
nekkan <nxkkdev@gmail.com>
2020-11-09 04:55:36 -0300

    Set up Ktor dependency at build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 kotlin {
+
     jvm {
         compilations.all {
             kotlinOptions.jvmTarget = "1.8"
@@ -20,6 +21,7 @@ kotlin {
     }
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
+
     val nativeTarget = when {
         hostOs == "Mac OS X" -> macosX64("native")
         hostOs == "Linux" -> linuxX64("native")
@@ -27,11 +29,11 @@ kotlin {
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
-    
     sourceSets {
         val commonMain by getting
         val commonTest by getting {
             dependencies {
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
             }
